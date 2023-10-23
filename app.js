c583a9994781af2d52a45e0547ba3ce0e384bb9b
hjy0951 <hjy0951@gmail.com>
2023-10-23 21:53:42 +0900

    배포

--- app.js
+++ app.js
@@ -21,7 +21,12 @@ const app = express();
 passportConfig();
 
 //Cors 설정
-app.use(cors({ origin: true, credentials: true }));
+app.use(
+  cors({
+    origin: "https://record-log.vercel.app", // 허용할 도메인
+    credentials: true, // credentials 허용
+  })
+);
 
 //request 요청 URL과 처리 로직을 선언한 라우팅 모듈 매핑
 app.use(express.json());
