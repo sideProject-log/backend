7a450c1be029c8e25c8a8a871766a59115199e17
hjy0951 <hjy0951@gmail.com>
2023-10-23 18:40:21 +0900

    deploy test

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.send("Project Log API Server");
+  res.send("Project Log API Server 도영이가 배포함");
 });
 
 module.exports = router;
